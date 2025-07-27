8a2bf0bfcbf6b001a3fb0193486474c3241ff9f0
Fahad Ahmed <fahad838@gmail.com>
2025-07-27 11:55:52 +0530

    Create/generate GetFeedFollowForUser query to return list of all feeds bu user

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -11,3 +11,10 @@ SELECT
 FROM feed_follow_record
 INNER JOIN feeds ON feeds.id = feed_follow_record.feed_id
 INNER JOIN users ON users.id = feed_follow_record.user_id;
+
+-- name: GetFeedFollowForUser :many
+SELECT feed_follows.*, feeds.name AS feed_name, users.name AS user_name
+FROM feed_follows
+INNER JOIN feeds ON feeds.id = feed_follows.feed_id
+INNER JOIN users ON users.id = feed_follows.user_id
+WHERE feed_follows.user_id = $1;
