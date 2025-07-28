8b45c7f56d9d6c6956a08120174529defc8e9810
Fahad Ahmed <fahad838@gmail.com>
2025-07-28 17:55:53 +0530

    Create/generate MarkFeedFetched query that marks the lastest feed fetched

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -23,3 +23,8 @@ ON feeds.user_id = users.id;
 SELECT * 
 FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
