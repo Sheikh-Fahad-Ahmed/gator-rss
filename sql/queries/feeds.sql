eb4af407749845ef3887d102df6c56751dd2c253
Fahad Ahmed <fahad838@gmail.com>
2025-07-28 18:24:04 +0530

    Create GetNextFeedToFetch query to retrive nulls_first/oldest feeds in db

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -28,3 +28,9 @@ WHERE url = $1;
 UPDATE feeds
 SET last_fetched_at = NOW(), updated_at = NOW()
 WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
